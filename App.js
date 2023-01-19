5ec7eb7af2ff444693cfde1c253c4c15b9950463
Chelsea <chelsea@tripwiretech.com>
2023-01-19 21:10:21 +1300

    carousel

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import PagerView from 'react-native-pager-view';
+
+// npx expo install react-native-pager-view
 
 export default function App() {
+  const ref = useRef();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <PagerView 
+        style={styles.pager} 
+        ref={ref}
+        initialPage={0}
+        onPageScroll={(e) => console.log(e)}
+        onPageSelected={(e) => console.log(e)}
+        onPageScrollStateChanged={(e) => console.log(e)}
+      >
+        <View key="1">
+          <Text>Page 1</Text>
+        </View>
+        <View key="2">
+          <Text>Page 2</Text>
+        </View>
+      </PagerView>
+      <Button title="Go to Pg 1" onPress={() => ref.current?.setPage(0)} />
+      <Button title="Go to Pg 2" onPress={() => ref.current?.setPage(1)} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +38,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pager: {
+    flex: 1,
+    alignSelf: "stretch"
+  }
 });
